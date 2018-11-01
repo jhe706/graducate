751e87208df74820d160cf0ed83244f40f67333f
Molly Chen <mollychen@Mollys-MacBook-Pro-2.local>
2018-11-01 16:12:10 -0400

    Tried adding simple routing

--- src/main.js
+++ src/main.js
@@ -5,24 +5,44 @@ import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'firebaseui/dist/firebaseui.css'
 
+// import all other components that need routes
+import CreateProfile from './components/CreateProfile.vue';
+import Matches from './components/Matches.vue';
+
 Vue.config.productionTip = false
 Vue.use(Vuetify);
 
+// simple routing from scratch
+const NotFound = {
+  template: '<p>Page not found</p>'
+};
+
+const routes = {
+  '/': App,
+  '/createprofile': CreateProfile,
+  '/matches': Matches
+}
+
 new Vue({
   el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
   components: {
     App
   },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
   render: h => h(App)
-  /*
-  ALTERNATIVE SHORT/LONGHAND VERSIONS:
-  render: function createElement(){
-    return createElement(App);
-  }
-  render(h){
-    return h(App);
-  }
-  render: h => h(App);
-  */
+  // render (h) {
+  //   return h(this.ViewComponent);
+  // }
+
+  // components: {
+  //   App
+  // },
+  // render: h => h(App)
 })
-// TODO: set up routing here if you choose to use it
