57f7dfd7066f32311d6005e5659797066ef290ec
Molly Chen <m.swimstar@gmail.com>
2018-11-14 11:13:34 -0500

    Update Profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -6,75 +6,78 @@
             <v-layout row wrap>
                 <!--Default cover photo-->
                 <v-flex xs12>
-                    <img src="../assets/coverphoto.jpg" height="200px" width="1090px"/>
+                    <!-- <img :src="imgUrl" height="200px" width="1090px"/> -->
+                    <img src="../assets/coverphoto.jpg" height="200px" width="100%"/>
                 </v-flex>
 
-                <!--Profile picture-->
-                <v-flex xs12>
-                    <v-avatar style="float:left; margin-left:20px">
-                        <img
-                        src="https://cdn.vuetifyjs.com/images/john.jpg"
-                        alt="Profile picture"
+                    <!--Profile picture-->
+                    <v-flex xs12>
+                        <!-- <v-btn @click="getPicURL()">URL</v-btn> -->
+                        <!--TODO: add img binding-->
+                        <v-avatar style="float:left; margin-left:20px">
+                            <img
+                            src="https://cdn.vuetifyjs.com/images/john.jpg"
+                            alt="Profile picture"
                         >
                     </v-avatar>
-                </v-flex>
+                    </v-flex>
 
-                <!--Name + Summary-->
-                <v-flex xs12 style="height:150px">
-                    <ul style="float:left; text-align:left">
-                        <li>
-                            <h2>{{user.firstName}} {{user.lastName}}</h2>
-                        </li>
-                        <li>Trinity, Class of {{user.gradYear}}</li>
-                        <li v-for="degree in user.degrees" :key="degree">{{degree.type}} {{degree.major}}</li>
-                        <li v-if="!user.hometown.state">From {{user.hometown.city}}, {{user.hometown.country}}</li>
-                        <li v-else>From {{user.hometown.city}}, {{user.hometown.state}}, {{user.hometown.country}}</li>
-                    </ul>
-                </v-flex>
+                    <!--Name + Summary-->
+                    <v-flex xs12 style="height:150px">
+                        <ul style="float:left; text-align:left">
+                            <li>
+                                <h2>{{user.firstName}} {{user.lastName}}</h2>
+                            </li>
+                            <li>Trinity, Class of {{user.gradYear}}</li>
+                            <li v-for="degree in user.degrees" :key="degree">{{degree.type}} {{degree.major}}</li>
+                            <li v-if="!user.hometown.state">From {{user.hometown.city}}, {{user.hometown.country}}</li>
+                            <li v-else>From {{user.hometown.city}}, {{user.hometown.state}}, {{user.hometown.country}}</li>
+                        </ul>
+                    </v-flex>
 
-                <!--Row 1-->
-                <v-flex xs6>
-                    <v-card class="profile-text">
-                        <v-card-text class="left-margin px-0">
-                            <h3>Intro</h3>
-                            {{user.bio}}
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-                <v-flex xs6>
-                    <v-card class="profile-text">
-                        <v-card-text class="left-margin px-0">
-                            <h3>Contact</h3>
-                            <ul class="square">
-                                <li>Email: {{user.email}}</li>
-                                <li>Phone: {{user.phoneNumber}}</li>
-                            </ul>
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
+                    <!--Row 1-->
+                    <v-flex xs6>
+                        <v-card class="profile-text">
+                            <v-card-text class="left-margin px-0">
+                                <h3>Intro</h3>
+                                {{user.bio}}
+                            </v-card-text>
+                        </v-card>
+                    </v-flex>
+                    <v-flex xs6>
+                        <v-card class="profile-text">
+                            <v-card-text class="left-margin px-0">
+                                <h3>Contact</h3>
+                                <ul class="square">
+                                    <li>Email: {{user.email}}</li>
+                                    <li>Phone: {{user.phoneNumber}}</li>
+                                </ul>
+                            </v-card-text>
+                        </v-card>
+                    </v-flex>
 
-                <!--Row 2-->
-                <v-flex xs6>
-                    <v-card class="profile-text">
-                        <v-card-text class="left-margin px-0">
-                            <h3 v-if="user.status==='Undergraduate'">{{user.firstName}} would like advice on: </h3>
-                            <h3 v-else>{{user.firstName}} would like to help with: </h3>
-                            <ul class="square" v-for="topic in user.advice" :key="topic">
-                                <li>{{topic}}</li>
-                            </ul>
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-                <v-flex xs6>
-                    <v-card class="profile-text">
-                        <v-card-text class="left-margin px-0">
-                            <h3>Interests</h3>
-                            <ul class="square" v-for="interest in user.interests" :key="interest">
-                                <li>{{interest}}</li>
-                            </ul>
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
+                    <!--Row 2-->
+                    <v-flex xs6>
+                        <v-card class="profile-text">
+                            <v-card-text class="left-margin px-0">
+                                <h3 v-if="user.status==='Undergraduate'">{{user.firstName}} would like advice on: </h3>
+                                <h3 v-else>{{user.firstName}} would like to help with: </h3>
+                                <ul class="square" v-for="topic in user.advice" :key="topic">
+                                    <li>{{topic}}</li>
+                                </ul>
+                            </v-card-text>
+                        </v-card>
+                    </v-flex>
+                    <v-flex xs6>
+                        <v-card class="profile-text">
+                            <v-card-text class="left-margin px-0">
+                                <h3>Interests</h3>
+                                <ul class="square" v-for="interest in user.interests" :key="interest">
+                                    <li>{{interest}}</li>
+                                </ul>
+                            </v-card-text>
+                        </v-card>
+                    </v-flex>
             </v-layout>
         </v-container>
     </v-card>
@@ -85,22 +88,23 @@
 /* eslint-disable */
 import {
     userRef,
-    db
+    db,
+    storageRef
 } from "../database";
 import * as areasOfStudy from "../assets/areasOfStudy.js";
 
 export default {
     name: "Profile",
-    components: {
-    },
+    components: {},
     data() {
         return {
+            imgUrl: ""
         };
     },
-    computed: {
-    },
+    computed: {},
     firebase: {
         users: userRef,
+        storage: storageRef
     },
     methods: {
         typeOfAdvice() {
@@ -109,23 +113,50 @@ export default {
             } else {
                 return user.name + " would like to give advice about: ";
             }
+        },
+
+        getPicURL() {
+            let imgRef = storageRef.child("images");
+            imgRef.getDownloadURL().then(function (url) {
+                // TODO: ?????
+                // Insert url into an <img> tag to "download"
+                console.log(url);
+            }).catch(function (error) {
+                switch (error.code) {
+                    case 'storage/object-not-found':
+                        // File doesn't exist
+                        break;
+                    case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break;
+                    case 'storage/canceled':
+                        // User canceled the upload
+                        break;
+                    case 'storage/unknown':
+                        // Unknown error occurred, inspect the server response
+                        break;
+                }
+            });
+            // let url = imageRef.getDownloadURL(url)       // doesnt work
+            // console.log(url);
         }
     },
     props: ['user']
 };
 </script>
+
 <style>
-    .profile-text {
-        height:150px; 
-        margin:10px; 
-        text-align:left;
-    }
+.profile-text {
+    height: 150px;
+    margin: 10px;
+    text-align: left;
+}
 
-    .left-margin {
-        margin-left: 15px;
-    }
+.left-margin {
+    margin-left: 15px;
+}
 
-    ul.square {
-        list-style-type: square;
-    }
+ul.square {
+    list-style-type: square;
+}
 </style>
